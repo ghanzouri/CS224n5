107af99304603de3fd46e4e134bfec2b3c9d4dbb
Ilies <ghanz@stanford.edu>
2021-02-22 22:23:59 +0100

    Commit

--- src/attention.py
+++ src/attention.py
@@ -106,6 +106,7 @@ class SynthesizerAttention(nn.Module):
         att = F.softmax(att, dim=-1)
         att = self.attn_drop(att)
         print("AAAAAAAAAAAAAA", att.shape)
+        print("BBBBBBBBBBBBBB", v.shape)
         y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
 
